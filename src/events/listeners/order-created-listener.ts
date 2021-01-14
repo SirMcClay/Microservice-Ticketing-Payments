4dff095f5901636044fe0e6d910b2d8421dc42af
sirmcclay <mcclay.design@gmail.com>
2021-01-14 11:41:53 -0300

    Order created listener - Implementation for save order

--- src/events/listeners/order-created-listener.ts
+++ src/events/listeners/order-created-listener.ts
@@ -5,10 +5,22 @@ import {
 	Subjects,
 } from '@sirmctickets/commontickets';
 import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 	readonly subject = Subjects.OrderCreated;
 	queueGroupName = queueGroupName;
 
-	async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+	async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+		const order = Order.build({
+			id: data.id,
+			price: data.ticket.price,
+			status: data.status,
+			userId: data.userId,
+			version: data.version,
+		});
+		await order.save();
+
+		msg.ack();
+	}
 }
