de7d618577bf06bdcc36702b43b81195f4e693d3
sirmcclay <mcclay.design@gmail.com>
2021-01-14 12:11:50 -0300

    Order created listener - tests

--- src/events/listeners/__test__/order-created-listener.test.ts
+++ src/events/listeners/__test__/order-created-listener.test.ts
@@ -3,6 +3,7 @@ import { Message } from 'node-nats-streaming';
 import { OrderCreatedEvent, OrderStatus } from '@sirmctickets/commontickets';
 import { natsWrapper } from '../../../nats-wrapper';
 import { OrderCreatedListener } from '../order-created-listener';
+import { Order } from '../../../models/order';
 
 const setup = async () => {
 	const listener = new OrderCreatedListener(natsWrapper.client);
@@ -27,6 +28,20 @@ const setup = async () => {
 	return { listener, data, msg };
 };
 
-it('replicates the order info', async () => {});
+it('replicates the order info', async () => {
+	const { listener, data, msg } = await setup();
 
-it('acks the message', async () => {});
+	await listener.onMessage(data, msg);
+
+	const order = await Order.findById(data.id);
+
+	expect(order!.price).toEqual(data.ticket.price);
+});
+
+it('acks the message', async () => {
+	const { listener, data, msg } = await setup();
+
+	await listener.onMessage(data, msg);
+
+	expect(msg.ack).toHaveBeenCalled();
+});
