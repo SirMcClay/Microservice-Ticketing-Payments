ab4bcb2cfee9956a757340a9c1705ab5662fab3f
sirmcclay <mcclay.design@gmail.com>
2021-01-18 11:52:40 -0300

    Save a payment ids of new route return to track payemtns from orders

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -10,6 +10,7 @@ import {
 } from '@sirmctickets/commontickets';
 import { stripe } from '../stripe';
 import { Order } from '../models/order';
+import { Payment } from '../models/payment';
 
 const router = express.Router();
 
@@ -43,11 +44,16 @@ router.post(
 		// 	description: 'My First Test Charge (created for API docs)',
 		// });
 
-		await stripe.charges.create({
+		const charge = await stripe.charges.create({
 			currency: 'usd',
 			amount: order.price * 100,
 			source: token,
 		});
+		const payment = Payment.build({
+			orderId,
+			stripeId: charge.id,
+		});
+		await payment.save();
 
 		res.status(201).send({ success: true });
 	}
